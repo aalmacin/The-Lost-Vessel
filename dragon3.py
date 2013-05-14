b9f0ea0ed23387419d05e2712199040d12e478a1
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-05-14 14:33:09 -0400

    Finished the arrays

--- dragon3.py
+++ dragon3.py
@@ -2,37 +2,30 @@ import random
 import time
 
 def display_intro():
-  print ('You are on a planet full of dragons. In front of you,')
-  print ('you see two caves. In one cave, the dragon is friendly')
-  print ('and will share his treasure with you. The other dragon')
-  print ("is greedy and hungry, and will eat you on sight.\n")
+  print ("")
+  print ("")
+  print ("")
+  print ("\n")
 
-def choose_cave():
+def get_outcomes():
+  nodes = [[["N","N"],["N","N"]],[["N","N"],["N","N"]]]
+  nodes[random.randint(0,1)][random.randint(0,1)][random.randint(0,1)] = "P"
+  return nodes
+
+def choose_place(place):
   while True:
-    cave = raw_input('Which cave will you go into? (1 or 2): ')
-    if cave == '1' or cave == '2': break
-  return cave
-
-def check_cave(chosen_cave):
-  print ('You approach the cave...')
-  time.sleep(2)
-  print ('It is dark and spooky...')
-  time.sleep(2)
-  print ("A large dragon jumps out in front of you! He opens his jaws and...\n")
-  time.sleep(2)
-
-  friendly_cave = random.randint(1, 2)
-
-  if chosen_cave == str(friendly_cave):
-    print ('Gives you his treasure!')
-  else:
-    print ('Gobbles you down in one bite!')
+    place = raw_input("Which {} will you go into? (1 or 2): ".format(place))
+    if place == '1' or place == '2': break
+  return int(place)
 
 def main():
   while True:
     display_intro()
-    chosen_cave = choose_cave()
-    check_cave(chosen_cave)
+    first_chosen = choose_place("Bangin")
+    second_chosen = choose_place("Look")
+    third_chosen = choose_place("Quad")
+    outcomes = get_outcomes()
+    print(outcomes[first_chosen][second_chosen][third_chosen])
     if(not raw_input("Do you want to play again? (yes or no): ").lower() in ["yes","y"]): break
 
 if __name__ == "__main__": main()
