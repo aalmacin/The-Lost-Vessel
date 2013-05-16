dda561c5f13e0f632d800d8e914fdaec34d4eed5
Aldrin Jerome Almacin <julienaldrin@gmail.com>
2013-05-16 11:13:19 -0400

    Added header for 2nd version

--- theShipGame2.py
+++ theShipGame2.py
@@ -1,3 +1,9 @@
+''' Author Name: Aldrin Jerome Almacin
+    Last Modified by: Aldrin Jerome Almacin
+    Date last Modified:
+    Program description: A game that asks the user to select paths that would lead to finding a treasure. There are 7 negative outcomes and one positive outcome. The player can then play the game again if desired.
+    Revision History: 0.2
+'''
 import random
 import time
 
